48e089520b4456a18de895c2a0931aeda40a8b24
hydaline-code <djougancharlen@gmail.com>
2023-12-02 17:43:32 +0100

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,8 +1,7 @@
 class Solver
-
   def factorial(number)
     raise StandardError, 'Input must be a positive integer' unless number.is_a?(Integer) && number >= 0
-  
+
     (1..number).reduce(1, :*)
   end
 
@@ -13,16 +12,14 @@ class Solver
   def fizzbuzz(number)
     raise ArgumentError, 'Input must be an integer' unless number.is_a?(Integer)
 
-    result =
-      if (number % 3).zero? && (number % 5).zero?
-        'fizzbuzz'
-      elsif (number % 3).zero?
-        'fizz'
-      elsif (number % 5).zero?
-        'buzz'
-      else
-        number.to_s
-      end
-    result
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
   end
 end
