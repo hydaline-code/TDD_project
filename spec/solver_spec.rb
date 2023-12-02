1a223a5e96ee965c771e14ececc892be4e29a673
hydaline-code <djougancharlen@gmail.com>
2023-12-02 17:17:19 +0100

    Add fizz buzz test cases

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -40,4 +40,31 @@ RSpec.describe Solver do
       expect(result).to eq('')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when N is divisible by 3' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when N is divisible by 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when N is divisible by both 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'returns N as a string for other cases' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+      expect(solver.fizzbuzz(11)).to eq('11')
+    end
+  end
+
 end
