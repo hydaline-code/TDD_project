74fd81df53e9485e59feccba0119049763ce5d61
hydaline-code <djougancharlen@gmail.com>
2023-12-02 17:24:07 +0100

    Add fizz buzz

--- solver.rb
+++ solver.rb
@@ -10,5 +10,18 @@ class Solver
     def reverse(word)
         word.reverse
     end
+    def fizzbuzz(n)
+      result =
+        if (n % 3).zero? && (n % 5).zero?
+          'fizzbuzz'
+        elsif (n % 3).zero?
+          'fizz'
+        elsif (n % 5).zero?
+          'buzz'
+        else
+          n.to_s
+        end
+      result
+    end
   end
   
